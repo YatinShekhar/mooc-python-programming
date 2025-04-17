7f5908a06a58dfeb3f6859f9201f7cdb4ee957b5
Yatin Shekhar Rikhari <yatinshekhar@gmail.com>
2025-04-17 19:44:26 +0530

    Update Part 1

---
+++ Part 1/1. Getting Started/Print_Some_Code.py
@@ -0,0 +1,11 @@
+'''
+================================================================
+
+Please write a program which prints out the following:
+
+print("Hello there!")
+
+================================================================
+'''
+
+print('print("Hello there!")')
